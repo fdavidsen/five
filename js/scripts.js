cd03cca63a7cab3b29d9273082b4b31ef4bd9fc7
fdavidsen <f.davidsen19@gmail.com>
2021-02-25 14:52:07 +0700

    Use proper logo and add menu image

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
+const menuName = {
+  'home'        : 'Home',
+  'member'      : 'Our Members',
+  'photo'       : 'Our Photos',
+  'video'       : 'Our Videos',
+  'contributor' : 'Contributors'
+}
+
 $('a[data-section]').on('click', function() {
   const section = $(this).data('section');
+
   $('section#' + section).addClass('active').siblings('section').removeClass('active');
+  $('title').html('FIVE - ' + menuName[section]);
   
   if (section === 'home') {
     $('.btn-home').css('display', 'none')
